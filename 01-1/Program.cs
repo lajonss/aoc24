12cf908db92c769bd10b3fb079c90dce9312cc19
lajonss <l3n1@dupaw.eu>
2024-12-07 21:13:03 +0100

    01-1

---
+++ 01-1/Program.cs
@@ -0,0 +1,33 @@
+﻿List<int> left = new();
+List<int> right = new();
+
+ParseInput();
+
+var sum = left.Order().Zip(right.Order(), Distance).Sum();
+Console.WriteLine(sum.ToString());
+
+
+void ParseInput() {
+    foreach (var line in GetLines())
+        ParseLine(line);
+    return;
+
+    IEnumerable<string> GetLines() {
+        string? line;
+        while ((line = Console.In.ReadLine()) != null)
+            yield return line;
+    }
+
+    void ParseLine(string line) {
+        if (string.IsNullOrWhiteSpace(line))
+            return;
+
+        var elements = line.Split(separator: ' ', options: StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        left.Add(int.Parse(elements[0]));
+        right.Add(int.Parse(elements[1]));
+    }
+}
+
+int Distance(int a, int b)
+    => Math.Abs(a - b);
+
