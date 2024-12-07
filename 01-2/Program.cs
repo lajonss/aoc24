c6c8038bee31404e31e3fbcc269272153566d462
lajonss <l3n1@dupaw.eu>
2024-12-07 21:27:05 +0100

    01-2

---
+++ 01-2/Program.cs
@@ -0,0 +1,32 @@
+﻿List<int> left = new();
+List<int> right = new();
+
+ParseInput();
+
+var similarity = left.Select(SimilarityScore).Sum();
+Console.WriteLine(similarity.ToString());
+
+void ParseInput() {
+    foreach (var line in GetLines())
+        ParseLine(line);
+    return;
+
+    IEnumerable<string> GetLines() {
+        string? line;
+        while ((line = Console.In.ReadLine()) != null)
+            yield return line;
+    }
+
+    void ParseLine(string line) {
+        if (string.IsNullOrWhiteSpace(line))
+            return;
+
+        var elements = line.Split(separator: ' ', options: StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        left.Add(int.Parse(elements[0]));
+        right.Add(int.Parse(elements[1]));
+    }
+}
+
+int SimilarityScore(int value) {
+    return value * right.Count(r => r == value);
+}
