2115b7c84304158bcdb8579f56e90abfc09db4c4
lajonss <l3n1@dupaw.eu>
2024-12-08 22:49:02 +0100

    03-1

---
+++ 03-1/Program.cs
@@ -0,0 +1,64 @@
+﻿var sum = 0;
+Phase phase = WaitForM;
+foreach (var character in GetInput())
+    phase = phase(character);
+Console.WriteLine(sum.ToString());
+
+IEnumerable<char> GetInput() {
+    while (true) {
+        var value = Console.Read();
+        if (value < 0)
+            yield break;
+        yield return Convert.ToChar(value);
+    }
+}
+
+Phase WaitForM(char character) {
+    if (character == 'm')
+        return WaitForU;
+    return WaitForM;
+}
+
+Phase WaitForU(char character) {
+    if (character == 'u')
+        return WaitForL;
+    return WaitForM;
+}
+
+Phase WaitForL(char character) {
+    if (character == 'l')
+        return WaitForOpeningBrace;
+    return WaitForM;
+}
+
+Phase WaitForOpeningBrace(char character) {
+    if (character == '(')
+        return ParseNumberFirst();
+    return WaitForM;
+}
+
+Phase ParseNumberFirst(int number = 0) {
+    return (character) => {
+        if (character == ',')
+            return ParseNumberSecond(firstNumber: number);
+        if (char.IsDigit(character) && number < 1000)
+            return ParseNumberFirst(Advance(number, character));
+        return WaitForM;
+    };
+}
+
+Phase ParseNumberSecond(int firstNumber, int secondNumber = 0) {
+    return (character) => {
+        if (character == ')')
+            sum += firstNumber * secondNumber;
+        else if (char.IsDigit(character) && secondNumber < 1000)
+            return ParseNumberSecond(firstNumber, Advance(secondNumber, character));
+        return WaitForM;
+    };
+}
+
+int Advance(int sum, char digit) {
+    return sum * 10 + (int)char.GetNumericValue(digit);
+}
+
+delegate Phase Phase(char character);
